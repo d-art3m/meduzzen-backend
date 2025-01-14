15ada8efa975916d19587ab0711aa9a80ec5a880
d-art3m <didenko.art3m@gmail.com>
2025-01-14 22:36:28 +0200

    feat: add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Health Check',
+    description: 'Checks if the app is running',
+  })
   healthCheck() {
     return this.appService.healthCheck();
   }
